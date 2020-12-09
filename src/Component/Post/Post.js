7f67d24eeca0e910ed8215179814cf37c38ede49
niemet0502 <vincentmarius8@gmail.com>
2020-12-09 08:03:29 +0100

    main :: align post item

--- src/Component/Post/Post.js
+++ src/Component/Post/Post.js
@@ -12,26 +12,28 @@ import PublishIcon from '@material-ui/icons/Publish';
 function Post({displayName, username, verified, text, image, avatar}) {
     return (
         <div className="post">
-            <div className="post_avatar">
-                <Avatar src={profil} />
-            </div>
-            <div className="post_body">
-                <div className="post_header">
-                    <div className="post_headerText">
-                        <h3>Marius NIEMET {" "}
-                            <span> <VerifiedUserIcon /> </span> </h3>
-                    </div>
-                    <div className="post_headerDescription">
-                        <p>Twitter Clone by Marius</p>
-                    </div>
+                <div className="post_avatar">
+                    <img className="Suggestion_image" src={profil} />
                 </div>
-                <img src="https://i.pinimg.com/564x/67/32/31/673231d0fecd5ba8b4be1678a3d732cf.jpg" alt=""/>
-            </div>
-            <div className="post_footer">
-                <ChatBubbleOutlineIcon fontSize="small" />
-                <RepeatIcon fontSize="small" />
-                <FavoriteBorderIcon fontSize="small" />
-                <PublishIcon fontSize="small" />
+            <div style={{border: "1px solid red"}}>
+                <div className="post_body">
+                        <div className="post_header">
+                            <div className="post_headerText">
+                                <h3>Marius NIEMET {" "}
+                                    <span> <VerifiedUserIcon /> </span> </h3>
+                            </div>
+                            <div className="post_headerDescription">
+                                <p>Twitter Clone by Marius</p>
+                            </div>
+                        </div>
+                        <img src="https://i.pinimg.com/564x/67/32/31/673231d0fecd5ba8b4be1678a3d732cf.jpg" alt=""/>
+                    </div>
+                <div className="post_footer">
+                    <ChatBubbleOutlineIcon fontSize="small" />
+                    <RepeatIcon fontSize="small" />
+                    <FavoriteBorderIcon fontSize="small" />
+                    <PublishIcon fontSize="small" />
+                </div>
             </div>
         </div>
     )
