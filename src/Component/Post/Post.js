d9258fa287660056a617ff64ba3c030d4570f34e
niemet0502 <vincentmarius8@gmail.com>
2020-12-17 07:55:41 +0100

    main :: created message component

--- src/Component/Post/Post.js
+++ src/Component/Post/Post.js
@@ -9,18 +9,20 @@ import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import PublishIcon from '@material-ui/icons/Publish';
 
 
-function Post({displayName, username, verified, text, image, avatar}) {
+function Post({displayName, username, verified, text, image, avatar, time}) {
     return (
         <div className="post">
                 <div className="post_avatar">
-                    <img className="Suggestion_image" src={profil} />
+                    <img className="Suggestion_image" src="https://pbs.twimg.com/profile_images/1325577759033339905/w2UOSqKh_400x400.jpg" />
                 </div>
             <div style={{border: "1px solid red"}}>
                 <div className="post_body">
                         <div className="post_header">
                             <div className="post_headerText">
-                                <h3>Marius NIEMET {" "}
+                                <h3  className="suggestion_name"> マリウス {" "}
                                     <span> <VerifiedUserIcon /> </span> </h3>
+                                    <h5 className="Suggestion_pseudo">@mariusniemet05</h5>
+                                    <h6 className="Suggestion_pseudo">· 12 déc.</h6>
                             </div>
                             <div className="post_headerDescription">
                                 <p>Twitter Clone by Marius</p>
